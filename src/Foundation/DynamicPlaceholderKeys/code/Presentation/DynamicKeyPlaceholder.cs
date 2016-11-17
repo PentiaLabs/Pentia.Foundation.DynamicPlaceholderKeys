138c688c0f3bb17699653f922dd5223463d8c058
Emil Klein <Hannibal.okkels@gmail.com>
2016-11-17 09:41:08 +0100

    Update DynamicKeyPlaceholder.cs

--- src/Foundation/DynamicPlaceholderKeys/code/Presentation/DynamicKeyPlaceholder.cs
+++ src/Foundation/DynamicPlaceholderKeys/code/Presentation/DynamicKeyPlaceholder.cs
@@ -1,27 +1,3 @@
-﻿#region Copyright (c) Pentia, 1997 - 2015. All rights reserved (R).
-
-// *************************************************************************
-// *                                                                       *
-// * http://www.pentia.net/                                                *
-// *                                                                       *
-// * E-Mail: info@pentia.net                                               *
-// *                                                                       *
-// * Copyright(C) Pentia, 1997 - 2015. All rights reserved (R).            *
-// *                                                                       *
-// * LEGAL NOTICE: This is unpublished proprietary source code of Pentia.  *
-// * The contents of this file are protected by copyright laws and         *
-// * international copyright treaties, as well as other intellectual       *
-// * property laws and treaties. These contents may not be extracted,      *
-// * copied, modified or redistributed either as a whole or part thereof   *
-// * in any form, and may not be used directly in, or to assist with, the  *
-// * creation of derivative works of any nature without prior written      *
-// * permission from Pentia. The above copyright notice does not           *
-// * evidence any actual or intended publication of this file.             *
-// *                                                                       *
-// *************************************************************************
-
-#endregion
-
 #region Dependencies
 
 using System.Web.UI;
